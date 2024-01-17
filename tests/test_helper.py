ed410fb3ef2afba9b998ce640d7bad1df290fba0
Vadim Zlatsin <vzlatsin@gmail.com>
2024-01-17 20:10:32 +1100

    basic test

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,5 +1,23 @@
-from src.helper import helper
+import unittest
+from unittest.mock import Mock, patch
+from src.helper import MyTradingApp  # Import your app
 
-def test_helper():
-    result = helper()
-    assert result == "I am helper!"
+class TestMyTradingApp(unittest.TestCase):
+    def setUp(self):
+        # This method is called before each test
+        # Set up your mock objects here
+        self.mock_tws = Mock()  # Creating a mock object to represent TWS
+
+    def test_connect_to_tws(self):
+        # Test the connect_to_tws method
+        with patch('src.helper.EClient.connect', self.mock_tws):
+            app = MyTradingApp()
+            app.connect_to_tws()
+
+            # Assert that connect was called with the correct parameters
+            self.mock_tws.assert_called_with("127.0.0.1", 7496, 0)
+
+    # Add more tests for other methods
+
+if __name__ == '__main__':
+    unittest.main()
