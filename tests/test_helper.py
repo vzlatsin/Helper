2a06305b144e2c6baec691867aaa392430b934e9
Vadim Zlatsin <vzlatsin@gmail.com>
2024-01-21 18:37:41 +1100

    internals tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,21 +1,23 @@
 import unittest
 from unittest.mock import Mock, patch
 from src.helper import MyTradingApp  # Import your app
+import logging
 
 class TestMyTradingApp(unittest.TestCase):
-    def setUp(self):
-        # This method is called before each test
-        # Set up your mock objects here
-        self.mock_tws = Mock()  # Creating a mock object to represent TWS
 
-    def test_connect_to_tws(self):
-        # Test the connect_to_tws method
-        with patch('src.helper.EClient.connect', self.mock_tws):
-            app = MyTradingApp()
-            app.connect_to_tws()
+    @patch('ibapi.client.EClient.__init__')
+    def test_initialization(self, mock_init):
+        logging.info("testing initialization")
+        app = MyTradingApp();
+        mock_init.assert_called_with(app,app)
 
-            # Assert that connect was called with the correct parameters
-            self.mock_tws.assert_called_with("127.0.0.1", 7496, 0)
+    @patch('ibapi.client.EClient.connect')
+    def test_connect_to_tws(self, mock_connect):
+        app = MyTradingApp();
+        app.connect_to_tws()
+
+        # Assert that connect was called with the correct parameters
+        mock_connect.assert_called_with("127.0.0.1", 7496, 0)
 
     # Add more tests for other methods
 
