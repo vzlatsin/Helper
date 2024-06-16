d28eeef13c341ee5361967c20c540d5f9a3ead53
Vadim Zlatsin <vzlatsin@gmail.com>
2024-06-16 20:23:33 +1000

    integrated new time management code into web app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TimeManagement from './components/TimeManagement';
+
+ReactDOM.render(<TimeManagement />, document.getElementById('time-management-root'));
